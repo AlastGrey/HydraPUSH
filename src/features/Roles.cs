bc538ef577f7dac26aacb9c5f54d41539ef17e62
Mr Diamond <49098391+MrDiamond64@users.noreply.github.com>
2026-08-19 20:06:22 -0500

    fix: Avoid Match Info Button and Chat button overlapping
    Nobody will notice... right?

--- src/features/Roles.cs
+++ src/features/Roles.cs
@@ -16,6 +16,15 @@ namespace HydraMenu.features
 
 			if(SkipSabotageChecks.SabotageAsCrewmate) HudManager.Instance.SabotageButton.gameObject.SetActive(true);
 			if(AllowVentingForCrewmates) HudManager.Instance.ImpostorVentButton.gameObject.SetActive(true);
+
+			// The Chat button and Match Info buttons will overlap if both are active in-game (but not in meetings)
+			// I tried modifying `MatchInfoButton.transform.position` and the likes to try and shift the button towards the left
+			// however that only moved the collider of the button, not the icon
+			// So we just use this workaround to hide the Match Info Button in situations where it will not overlap with the Chat button
+			if(Chat.AlwaysVisibleChat.Enabled)
+			{
+				HudManager.Instance.MatchInfoButton.gameObject.SetActive(MeetingHud.Instance != null);
+			}
 		}
 
 		[HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.CmdCheckShapeshift))]
